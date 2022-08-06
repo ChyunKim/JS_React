af01a2881a31a4182b48f2b936b18748e2ba8a51
ChyunKim <co1995@naver.com>
2022-08-06 22:23:56 +0900

    docs: Study React

--- week2/EX1.js
+++ week2/EX1.js
@@ -34,10 +34,14 @@ const App = () => {
     }
 
     const handleClick = () => {
-        alertId !== '' && focusId.current.focus() || setId('')
-        alertPwd !== '' && focusPwd.current.focus() || setPwd('')
-        alertId === '' && alertPwd === '' && alert('회원가입 성공!');
-    };
+        if(alertId !== '') {
+            focusId.current.focus()
+            setId('')
+        }else if(alertPwd !== '') {
+            focusPwd.current.focus()
+            setPwd('')
+        }else alert('회원가입 성공!');
+    }
     
     return (
         <div>
